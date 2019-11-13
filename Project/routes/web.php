19d7aa290f5e10b4e87ca60c568cd7a3fbc5d722
iDols Not <notpn2017@gmail.com>
2019-11-13 09:08:28 +0700

    Up some changes

--- Project/routes/web.php
+++ Project/routes/web.php
@@ -12,5 +12,11 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+Route::get('/contact', function () {
+    return view('pages.contact');
+});
+Route::get('/about', function () {
+    return view('pages.about');
 });
